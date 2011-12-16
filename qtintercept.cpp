91e930fe9273bf1e3754683fd38dc88b09624d53
Nikita Tsukanov <keks9n@gmail.com>
2011-12-17 03:39:55 +0400

    fixed license banner position

--- qtintercept.cpp
+++ qtintercept.cpp
@@ -1,3 +1,24 @@
+/*
+qintercept.cpp
+Author:
+     Nikita Tsukanov <keks9n@gmail.com>
+
+Copyright (c) 2011 Nikita Tsukanov
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <QtGui/QSystemTrayIcon>
 #include <QList>
 #include "skypetab.h"
@@ -75,26 +96,7 @@ bool QObject::connect ( const QObject * sender, const char * signal, const QObje
 
 typedef void (QWidget::*setVisibleProto)(bool visible);
 setVisibleProto realSetVisible=0;
-/*
-qintercept.cpp
-Author:
-     Nikita Tsukanov <keks9n@gmail.com>
 
-Copyright (c) 2011 Nikita Tsukanov
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 extern void QWidget::setVisible(bool visible)
 {
