af191e085be43040dcc93cdd03a2743c8f0f08d2
Nikita Tsukanov <keks9n@gmail.com>
2011-12-19 05:04:54 +0400

    Some fixes for gendeb.sh

--- gendeb.sh
+++ gendeb.sh
@@ -3,7 +3,7 @@ VERSION=`grep VERSION skypetab-ng.pro|sed "s/.*= *//"`
 dpkg-buildpackage -S
 sed -i "s/$VERSION-1) oneiric/$VERSION-1natty) natty/" debian/changelog
 dpkg-buildpackage -S
-sed -i "s/natty/maverick/" debian/changelog
+sed -i "s/natty/maverick/g" debian/changelog
 dpkg-buildpackage -S
-sed -i "s/maverick/lucid/" debian/changelog
+sed -i "s/maverick/lucid/g" debian/changelog
 dpkg-buildpackage -S
