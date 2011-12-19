52a7c069576342e3917a5c5e88d37b22a7070a2f
Nikita Tsukanov <keks9n@gmail.com>
2011-12-19 04:54:06 +0400

    Added a small script for debian source package generation

---
+++ gendeb.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+VERSION=`grep VERSION skypetab-ng.pro|sed "s/.*= *//"`
+dpkg-buildpackage -S
+sed -i "s/$VERSION-1) oneiric/$VERSION-1natty) natty/" debian/changelog
+dpkg-buildpackage -S
+sed -i "s/natty/maverick/" debian/changelog
+dpkg-buildpackage -S
+sed -i "s/maverick/lucid/" debian/changelog
+dpkg-buildpackage -S
