5a0e4b90890b472c28e66f594282a81290366fc4
Nikita Tsukanov <keks9n@gmail.com>
2011-12-20 12:06:32 +0400

    Unset some env variables to fix segfaults because of GTK+ theme loader

--- gendeb.sh
+++ gendeb.sh
@@ -3,6 +3,11 @@ rm -rf .git
 VERSION=`grep VERSION skypetab-ng.pro|sed "s/.*= *//"`
 dpkg-buildpackage -S
 sed -i "s/$VERSION-1) oneiric/$VERSION-1natty) natty/" debian/changelog
+
+#This fix is required because of the broken GTK theme loader
+sed -i "s/sh$/sh\nunset GNOME_DISPLAY_SESSION_ID/" skypetab-ng
+sed -i "s/sh$/sh\nunset DESKTOP_SESSION/" skypetab-ng
+
 dpkg-buildpackage -S
 sed -i "s/natty/maverick/g" debian/changelog
 dpkg-buildpackage -S
