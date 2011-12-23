4d937a5116f4b3e993091fff49cffb34a1306dd8
Nikita Tsukanov <keks9n@gmail.com>
2011-12-23 22:06:17 +0400

    Added some scripts for RPM packging Note there is no any dependency tracking

---
+++ genrpm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+VERSION=`grep VERSION skypetab-ng.pro|sed "s/.*= *//"`
+echo Preparing environment
+rm -rf tmprpm
+mkdir -p tmprpm/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+mkdir -p tmprpm/skypetab-ng-$VERSION
+cp -R * tmprpm/skypetab-ng-$VERSION
+cd tmprpm
+tar czf skypetab-ng.tar.gz skypetab-ng-$VERSION
+mv skypetab-ng.tar.gz rpmbuild/SOURCES
+cp ../skypetab-ng.spec rpmbuild/SPECS
+echo Building
+rpmbuild -bs ../skypetab-ng.spec --buildroot `pwd`/build/ --define='%_topdir %(echo `pwd`)/rpmbuild'
+cp rpmbuild/SRPMS/* ../../
