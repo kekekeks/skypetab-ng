b337acf7f64cf664494f0a54816772685a38d787
Nikita Tsukanov <keks9n@gmail.com>
2012-05-06 12:46:07 +0400

    Removed call to SkypeTab::stage1Init from QObject::connect
    It seems to cause problems with clipboard in KDE, so we'll hope that
    connect() calls that we want to be intercepted will take place after
    stage1Init.
    ref #38

--- qtintercept.cpp
+++ qtintercept.cpp
@@ -84,7 +84,6 @@ bool QObject::connect ( const QObject * sender, const char * signal, const QObje
 		Dl_info nfo;
 		dladdr((void*)real_connect, &nfo);
 		real_connect=(connect_proto)dlsym(RTLD_NEXT, nfo.dli_sname);
-		SkypeTab::stage1Init();
 	}
 
 	if(sender!=0)
